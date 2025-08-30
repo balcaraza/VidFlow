9416d94a5c9f960d0aa5cfe84161a88b8d85a00b
Balam <balcaraza@gmail.com>
2025-08-30 16:48:26 -0600

    Feature | Se agrega paquete de prettier

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const videosContainer = document.querySelector('.videos__container')
-console.log(verde);
-
+const videosContainer = document.querySelector(".videos__container");
 
 async function fetchAndDisplayVideos() {
   try {
-    const fetchRequest = await fetch('http://localhost:3000/videos')
-    const videos = await fetchRequest.json()
+    const fetchRequest = await fetch("http://localhost:3000/videos");
+    const videos = await fetchRequest.json();
 
     videos.forEach((video) => {
-      if (video.categoria === '') {
-        throw new Error('El video no tiene categoría')
+      if (video.categoria === "") {
+        throw new Error("El video no tiene categoría");
       }
       videosContainer.innerHTML += `
                 <li class="videos__item">
@@ -21,57 +19,57 @@ async function fetchAndDisplayVideos() {
                         <p class="category" hidden>${video.category}</p>
                     </div>
                 </li>
-            `
-    })
+            `;
+    });
   } catch (error) {
-    videosContainer.innerHTML = `<p>Hubo un error al cargar los videos: ${error}</p>`
+    videosContainer.innerHTML = `<p>Hubo un error al cargar los videos: ${error}</p>`;
   }
 }
 
-fetchAndDisplayVideos()
+fetchAndDisplayVideos();
 
-const searchBar = document.querySelector('.search__input')
+const searchBar = document.querySelector(".search__input");
 
-searchBar.addEventListener('input', filterSearch)
+searchBar.addEventListener("input", filterSearch);
 
 function filterSearch() {
-  const videoItems = document.querySelectorAll('.videos__item')
+  const videoItems = document.querySelectorAll(".videos__item");
 
-  if (searchBar.value !== '') {
+  if (searchBar.value !== "") {
     for (let video of videoItems) {
-      let title = video.querySelector('.video-title').textContent.toLowerCase()
-      let filterValue = searchBar.value.toLowerCase()
+      let title = video.querySelector(".video-title").textContent.toLowerCase();
+      let filterValue = searchBar.value.toLowerCase();
 
       if (!title.includes(filterValue)) {
-        video.style.display = 'none'
+        video.style.display = "none";
       } else {
-        video.style.display = 'block'
+        video.style.display = "block";
       }
     }
   } else {
     for (let video of videoItems) {
-      video.style.display = 'block'
+      video.style.display = "block";
     }
   }
 }
 
-const categoryButtons = document.querySelectorAll('.top__item')
+const categoryButtons = document.querySelectorAll(".top__item");
 
 categoryButtons.forEach((button) => {
-  let categoryName = button.getAttribute('name')
-  button.addEventListener('click', () => filterByCategory(categoryName))
-})
+  let categoryName = button.getAttribute("name");
+  button.addEventListener("click", () => filterByCategory(categoryName));
+});
 
 function filterByCategory(filter) {
-  const videoItems = document.querySelectorAll('.videos__item')
+  const videoItems = document.querySelectorAll(".videos__item");
   for (let video of videoItems) {
-    let category = video.querySelector('.category').textContent.toLowerCase()
-    let filterValue = filter.toLowerCase()
+    let category = video.querySelector(".category").textContent.toLowerCase();
+    let filterValue = filter.toLowerCase();
 
-    if (!category.includes(filterValue) && filterValue !== 'todo') {
-      video.style.display = 'none'
+    if (!category.includes(filterValue) && filterValue !== "todo") {
+      video.style.display = "none";
     } else {
-      video.style.display = 'block'
+      video.style.display = "block";
     }
   }
 }
